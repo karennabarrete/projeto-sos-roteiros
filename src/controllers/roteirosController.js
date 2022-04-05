47d35b68946cb12b8fbfc06031afffd48402c902
Karen Nabarrete Garcia <seuemail@gmail.com>
2022-04-05 19:48:02 -0300

    erro bug

--- src/controllers/roteirosController.js
+++ src/controllers/roteirosController.js
@@ -1,4 +1,4 @@
-const Roteiros = require("../models/Roteiros.js");
+const Roteiros = require("../models/Roteiros");
 const Cidades = require ("../models/Cidades.js");
 const Eventos = require ("../models/Eventos.js");
 
