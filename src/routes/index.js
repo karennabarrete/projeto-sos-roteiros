d154379113d3f8323bbcc386a5c17d5b02a43d48
karennabarrete <karen.nabarrete.garcia@gmail.com>
2022-04-06 14:14:28 -0300

    teste de nova rota

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const roteirosController = require ("../controllers/roteirosController.js");
 const routes = express.Router();
 
 routes.get ("/roteiros", roteirosController.listarRoteiros);
-routes.get ("/eventos/:id", roteirosController.listarEventos);
+routes.get ("/roteiro/:id*", roteirosController.listarEventos);
 routes.get ("/cidades", roteirosController.listarCidades);
 
 module.exports = routes; 
