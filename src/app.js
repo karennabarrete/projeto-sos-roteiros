d798af07ffc725f528a59cad83f3616b9c66ce10
karennabarrete <karen.nabarrete.garcia@gmail.com>
2022-04-06 17:34:42 -0300

    add cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 const PORT = 8000;
 
@@ -9,6 +10,8 @@ const app = express();
 db.hasConection();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(routes);
        
